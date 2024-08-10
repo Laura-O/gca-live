f358bc00d1f81237785aadf40fe49a7b82099508
Laura Ohrndorf <laura.ohrndorf@gmail.com>
2024-08-11 00:01:50 +0200

    Initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    export let data;
+</script>
+
+<h1>Competitions</h1>
+
+{#each data.competitions as competition}
+    <h2>{competition.name}</h2>
+    <a
+        href="https://live.worldcubeassociation.org/link/competitions/{competition.id}"
+        >WCA Live</a
+    >
+    <a href="https://www.competitiongroups.com/competitions/{competition.id}"
+        >Einteilungen</a
+    >
+    <a
+        href="https://www.worldcubeassociation.org/competitions/{competition.id}#competition-schedule"
+        >Zeitplan</a
+    >
+    <a
+        href="https://www.worldcubeassociation.org/competitions/{competition.id}#general-info"
+        >Alle Infos</a
+    >
+{/each}
---
+++ src/routes/+page.ts
@@ -0,0 +1,20 @@
+export async function load({ fetch }) {
+    const response = await fetch('https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/competitions/DE.json');
+    const deResponse = await response.json();
+    const competitions = deResponse.items;
+
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    const nextWeek = new Date();
+    nextWeek.setDate(nextWeek.getDate() + 7);
+
+    const filteredCompetitions = competitions.filter(comp => {
+        const compDate = new Date(comp.date.from);
+        return compDate >= yesterday && compDate <= nextWeek;
+    });
+
+    return {
+        competitions: filteredCompetitions
+    };
+}
