73fe14578b32f82e865f96b0380747c91fa719bc
Laura Ohrndorf <laura.ohrndorf@gmail.com>
2024-08-11 13:54:16 +0200

    Make de default

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,7 +4,7 @@ import { loadTranslations } from '$lib/translations';
 export const load = async ({ url }) => {
     const { pathname } = url;
 
-    const initLocale = 'en'; // get from cookie, user session, ...
+    const initLocale = 'de'; // get from cookie, user session, ...
 
     await loadTranslations(initLocale, pathname); // keep this just before the `return`
 
