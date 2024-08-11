94d6b9be8c8e73e3518bebecdef8037aabc3bdab
Laura Ohrndorf <laura.ohrndorf@gmail.com>
2024-08-12 00:13:54 +0200

    Add daisyUI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,20 @@
     import { t, locales, locale } from "$lib/translations";
 </script>
 
-<select bind:value={$locale}>
-    {#each $locales as value}
-        <option {value}>{$t(`lang.${value}`)}</option>
-    {/each}
-</select>
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+        <a class="btn btn-ghost text-xl" href="/">GCA Live</a>
+    </div>
+    <div class="flex-none">
+        <select
+            bind:value={$locale}
+            class="select select-primary w-full max-w-xs"
+        >
+            {#each $locales as value}
+                <option {value}>{$t(`lang.${value}`)}</option>
+            {/each}
+        </select>
+    </div>
+</div>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,51 @@
 <script lang="ts">
     import { t } from "$lib/translations";
+    import "../app.css";
     export let data;
 </script>
 
-<h1>{$t("content.comp")}</h1>
-
-{#each data.competitions as competition}
-    <h2>{competition.name}</h2>
-    <a
-        href="https://live.worldcubeassociation.org/link/competitions/{competition.id}"
-        >WCA Live</a
-    >
-    <a href="https://www.competitiongroups.com/competitions/{competition.id}"
-        >{$t("content.grouping")}</a
-    >
-    <a
-        href="https://www.worldcubeassociation.org/competitions/{competition.id}#competition-schedule"
-        >{$t("content.schedule")}</a
-    >
-    <a
-        href="https://www.worldcubeassociation.org/competitions/{competition.id}#general-info"
-        >{$t("content.info")}</a
-    >
-{/each}
+<div
+    class="container mx-auto p-6 grid grid-cols-1 md:grid-cols-2 gap-4 place-items-center"
+>
+    {#each data.competitions as competition}
+        <div class="card-compact bg-base-100 w-96 shadow">
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">{competition.name}</h2>
+                <ul class="my-4 space-y-3 flex flex-col items-center">
+                    <li>
+                        <a
+                            role="button"
+                            class="btn btn-primary"
+                            href="https://live.worldcubeassociation.org/link/competitions/{competition.id}"
+                            >WCA Live</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                            role="button"
+                            class="btn btn-primary"
+                            href="https://www.competitiongroups.com/competitions/{competition.id}"
+                            >{$t("content.grouping")}</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                            role="button"
+                            class="btn btn-primary"
+                            href="https://www.worldcubeassociation.org/competitions/{competition.id}#competition-schedule"
+                            >{$t("content.schedule")}</a
+                        >
+                    </li>
+                    <li>
+                        <a
+                            role="button"
+                            class="btn btn-primary"
+                            href="https://www.worldcubeassociation.org/competitions/{competition.id}#general-info"
+                            >{$t("content.info")}</a
+                        >
+                    </li>
+                </ul>
+            </div>
+        </div>
+    {/each}
+</div>
