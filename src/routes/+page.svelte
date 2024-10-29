4d3f568669afdcb99783d2bb5d3f7b1a63832f1a
Laura Ohrndorf <laura.ohrndorf@gmail.com>
2024-10-30 00:02:54 +0100

    Add single competition route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { t } from '$lib/translations';
-	import '../app.css';
+	import CompetitionCard from '$lib/components/CompetitionCard.svelte';
 	export let data;
 </script>
 
@@ -13,56 +13,18 @@
 				<h2 class="card-title">{$t('content.nocomps-title')}</h2>
 				<p>{$t('content.nocomps-content')}</p>
 				<div class="card-actions justify-center">
-					<button class="btn btn-primary"
-						><a
+					<button class="btn btn-primary">
+						<a
 							href="https://www.worldcubeassociation.org/competitions?region=Germany&search=&state=present&year=all+years&from_date=&to_date=&delegate=&display=list"
 							>{$t('content.wca-page')}</a
-						></button
-					>
+						>
+					</button>
 				</div>
 			</div>
 		</div>
 	{:else}
 		{#each data.competitions as competition}
-			<div class="card-compact bg-base-100 w-96 shadow">
-				<div class="card-body items-center text-center">
-					<h2 class="card-title">{competition.name}</h2>
-					<ul class="my-4 space-y-3 flex flex-col items-center">
-						<li>
-							<a
-								role="button"
-								class="btn btn-primary"
-								href="https://live.worldcubeassociation.org/link/competitions/{competition.id}"
-								target="_blank">WCA Live</a
-							>
-						</li>
-						<li>
-							<a
-								role="button"
-								class="btn btn-primary"
-								href="https://www.competitiongroups.com/competitions/{competition.id}"
-								target="_blank">{$t('content.grouping')}</a
-							>
-						</li>
-						<li>
-							<a
-								role="button"
-								class="btn btn-primary"
-								href="https://www.worldcubeassociation.org/competitions/{competition.id}#competition-schedule"
-								target="_blank">{$t('content.schedule')}</a
-							>
-						</li>
-						<li>
-							<a
-								role="button"
-								class="btn btn-primary"
-								href="https://www.worldcubeassociation.org/competitions/{competition.id}#general-info"
-								target="_blank">{$t('content.info')}</a
-							>
-						</li>
-					</ul>
-				</div>
-			</div>
+			<CompetitionCard competition="{competition}" />
 		{/each}
 	{/if}
 </div>
---
+++ src/routes/competitions/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let data;
+	import CompetitionCard from '$lib/components/CompetitionCard.svelte';
+	const { competition } = data;
+</script>
+
+<div
+	class="container mx-auto p-6 grid grid-cols-1 md:grid-cols-2 gap-4 place-items-center"
+>
+	<CompetitionCard competition="{competition}" />
+</div>
