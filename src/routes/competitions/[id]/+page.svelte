3c9f696a7382e4183242aa04493d0813a57e75b1
Laura Ohrndorf <laura.ohrndorf@gmail.com>
2024-10-30 00:19:39 +0100

    Add qr lib and component

--- src/routes/competitions/[id]/+page.svelte
+++ src/routes/competitions/[id]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	export let data;
 	import CompetitionCard from '$lib/components/CompetitionCard.svelte';
+	import QRCode from '$lib/components/QRCode.svelte';
 	const { competition } = data;
 </script>
 
@@ -8,4 +9,7 @@
 	class="container mx-auto p-6 grid grid-cols-1 md:grid-cols-2 gap-4 place-items-center"
 >
 	<CompetitionCard competition="{competition}" />
+	<QRCode
+		url="{`https://live.germancubeassociation.de/competitions/${competition.id}`}"
+	/>
 </div>
