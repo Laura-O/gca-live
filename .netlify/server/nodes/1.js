c1a9113d540e35b0a4600e84691be199d665ee84
Laura Ohrndorf <laura.ohrndorf@gmail.com>
2025-12-07 22:19:51 +0100

    Fix Tailwind

--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BQsh7mBn.js","_app/immutable/chunks/D5OrjbLf.js","_app/immutable/chunks/CkPqafF2.js","_app/immutable/chunks/BThUvnxJ.js","_app/immutable/chunks/szi5wOji.js","_app/immutable/chunks/DlKhYTjz.js","_app/immutable/chunks/C687cqXg.js","_app/immutable/chunks/Cxi9DoPE.js"];
-export const stylesheets = [];
-export const fonts = [];
